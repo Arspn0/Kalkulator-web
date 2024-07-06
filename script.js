32edcf9a675d212e6a89e78c29e5ab6e01f9c641
Aris Sopian <arissopian0909@gmail.com>
2024-07-06 21:34:45 +0700

    perubahan total

--- script.js
+++ script.js
@@ -1,17 +1,96 @@
-const display = document.getElementById("display");
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator = null;
 
-function appendToDisplay(input){
-    display.value += input;
+const output = document.querySelector('.output');
+
+function buttonClick(value) {
+    if (isNaN(value)) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+
+    output.innerText = buffer;
 }
 
-function clearDisplay(){
-    display.value = "";
-}
+function handleSymbol(symbol) {
+    switch (symbol) {
+        case 'C':
+            buffer = '0';
+            runningTotal = 0;
+            previousOperator = null;
+            break;
+        case '=':
+            if (previousOperator === null) {
+                return;
+            }
 
-function calculate(){
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = "error";
+            flushOperation(parseInt(buffer.split(previousOperator)[1]));
+            previousOperator = null;
+            buffer = runningTotal.toString();
+            runningTotal = 0;
+            break;
+        case '←':
+            if (buffer.length === 1) {
+                buffer = '0';
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case '+':
+        case '-':
+        case '×':
+        case '÷':
+            handleMath(symbol);
+            break;
     }
 }
+
+function handleMath(symbol) {
+    if (buffer === '0') {
+        return;
+    }
+
+    if (previousOperator !== null) {
+        flushOperation(parseInt(buffer.split(previousOperator)[1]));
+    } else {
+        runningTotal = parseInt(buffer);
+    }
+
+    previousOperator = symbol;
+    buffer += ` ${symbol} `;
+}
+
+function flushOperation(intBuffer) {
+    switch (previousOperator) {
+        case '+':
+            runningTotal += intBuffer;
+            break;
+        case '-':
+            runningTotal -= intBuffer;
+            break;
+        case '×':
+            runningTotal *= intBuffer;
+            break;
+        case '÷':
+            runningTotal /= intBuffer;
+            break;
+    }
+}
+
+function handleNumber(numberString) {
+    if (buffer === '0') {
+        buffer = numberString;
+    } else {
+        buffer += numberString;
+    }
+}
+
+function init() {
+    document.querySelector('.bagian-tombol').addEventListener('click', function(event) {
+        buttonClick(event.target.innerText);
+    });
+}
+
+init();
